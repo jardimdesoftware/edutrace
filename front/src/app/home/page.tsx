98653dc37dbc1606145b1b4a3beaf4cabfce9a11
Ruan Carvalho <142753360+RuanCarvalhoo@users.noreply.github.com>
2026-09-21 09:08:55 -0300

    feat(front): renomear o atalho da home para Anotações Multiprofissionais
    Troca só o rótulo do atalho que leva a /comentarios, alinhado ao nome da
    tela.
    
    Refs #224

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -41,7 +41,7 @@ function HomePage() {
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               <HomeCard label="Triagem" icon={ClipboardPlus} href="/triagem" />
               <HomeCard label="Anamnese" icon={Stethoscope} href="/anamnese" />
-              <HomeCard label="Comentários Multiprofissionais" icon={MessagesSquare} href="/comentarios" />
+              <HomeCard label="Anotações Multiprofissionais" icon={MessagesSquare} href="/comentarios" />
               <HomeCard label="PEI" icon={BookOpenCheck} href="/pei" />
             </div>
           </>
